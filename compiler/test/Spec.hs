ec5f4583200aa4ef79333e7390e0ffaa54fa725b
Caleb Stimpson <caleb.stimpson@gmail.com>
2024-06-30 20:11:37 -0500

    Add first test

--- compiler/test/Spec.hs
+++ compiler/test/Spec.hs
@@ -1,2 +1,21 @@
+import Test.Hspec
+import Test.QuickCheck
+import Compiler.Tokenizer (tokenize, kind, content, Token(Token), TokenKind(..))
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Compiler.Tokenizer" $ do
+        it "export main:IO()\\t=of()\\n" $ do
+            tokenize "export main:IO()\t=of()\n" `shouldBe` [
+                    Token { kind = LetterIdentifier, content = "export" },
+                    Token { kind = InlineWhitespace, content = " " },
+                    Token { kind = LetterIdentifier, content = "main" },
+                    Token { kind = SymbolIdentifier, content = ":" },
+                    Token { kind = LetterIdentifier, content = "IO" },
+                    Token { kind = SymbolIdentifier, content = "()" },
+                    Token { kind = InlineWhitespace, content = "\t" },
+                    Token { kind = SymbolIdentifier, content = "=" },
+                    Token { kind = LetterIdentifier, content = "of" },
+                    Token { kind = SymbolIdentifier, content = "()" },
+                    Token { kind = EOL, content = "\n" }
+                ]
