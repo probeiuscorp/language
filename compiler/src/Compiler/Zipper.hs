fc588a4c56225675ccd2cee9b677f11bee7fe29d
Caleb Stimpson <caleb.stimpson@gmail.com>
2024-07-08 09:04:50 -0500

    Add custom list Zipper

---
+++ compiler/src/Compiler/Zipper.hs
@@ -0,0 +1,43 @@
+module Compiler.Zipper (Zipper(..), start, peek, left, right, eat, isDone, match, matchCond) where
+
+import Compiler.Tokenizer (mapFirst)
+
+data Zipper a = Zipper [a] [a] deriving (Eq, Show)
+
+start :: [a] -> Zipper a
+start = Zipper []
+
+peek :: Zipper a -> Maybe a
+peek (Zipper _ (x:_)) = Just x
+peek _                = Nothing
+
+right :: Zipper a -> Maybe (a, Zipper a)
+right (Zipper bt (x:xs)) = Just (x, Zipper (x:bt) xs)
+right _                  = Nothing
+
+left :: Zipper a -> Maybe (a, Zipper a)
+left (Zipper (x:xs) ft) = Just (x, Zipper xs $ x:ft)
+left _                  = Nothing
+
+eat :: (a -> Bool) -> Zipper a -> Zipper a
+eat predicate z = maybe z (\(x, zr) -> if predicate x
+  then eat predicate zr
+  else z) $ right z
+
+isDone :: Zipper a -> Bool
+isDone (Zipper _ ft) = null ft
+
+matchLoop :: (a -> Maybe b) -> ([b], Zipper a) -> ([b], Zipper a)
+matchLoop f m@(acc, Zipper bt (a:xs)) = case f a of
+  Just b -> matchLoop f (b:acc, Zipper (a:bt) xs)
+  Nothing -> m
+matchLoop _ m = m
+
+match :: (a -> Maybe b) -> Zipper a -> ([b], Zipper a)
+match f z = mapFirst reverse $ matchLoop f ([], z)
+
+filterMatch :: (a -> Bool) -> a -> Maybe a
+filterMatch f a = if f a then Just a else Nothing
+
+matchCond :: (a -> Bool) -> Zipper a -> ([a], Zipper a)
+matchCond f z = mapFirst reverse $ matchLoop (filterMatch f) ([], z)
