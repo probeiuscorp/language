34a4766a0f719c46555e8b11a5073fb8f511a393
Caleb Stimpson <caleb.stimpson@gmail.com>
2024-07-08 08:58:09 -0500

    Start compiler AST

---
+++ compiler/src/Compiler/AST.hs
@@ -0,0 +1,54 @@
+module Compiler.AST where
+
+type ValidIdentifier = String
+type PropertyKey = String
+
+data Abstraction = Abstraction
+    { binding :: PropertyKey
+    , constraint :: Maybe TypeExpression
+    , body :: Expression
+    } deriving (Eq, Show)
+data CallArgs a = CallArgs
+    { target :: a
+    , argument :: a
+    } deriving (Eq, Show)
+type ExpressionCallArgs = CallArgs Expression
+type TypeExpressionCallArgs = CallArgs TypeExpression
+data Expression =
+    Function Abstraction |
+    Application ExpressionCallArgs |
+    Identifier ValidIdentifier|
+    StringLiteral String |
+    NumberLiteral Double |
+    Record [Abstraction] deriving (Eq, Show)
+
+data TypeExpression =
+    TypeConstructor Abstraction |
+    TypeApplication TypeExpressionCallArgs |
+    TypeIdentifier ValidIdentifier deriving (Eq, Show)
+
+data Associativity = LeftAssociative | RightAssociative
+    deriving (Eq, Show)
+data InfixDeclaration = InfixDeclaration
+    { precedence :: Double
+    , infixIdent :: ValidIdentifier
+    , associativity :: Associativity
+    } deriving (Eq, Show)
+
+data DestructuringMember = DestructuringMember
+    { destructIdent :: ValidIdentifier
+    , destructBinding :: ValidIdentifier
+    } deriving (Eq, Show)
+type Destructuring = [DestructuringMember]
+data ImportListing = ImportAll | ImportOnly Destructuring | ImportHiding Destructuring | ImportAs ValidIdentifier
+    deriving (Eq, Show)
+
+data DeclarationModule = DeclarationModule
+    { identifier :: ValidIdentifier
+    , isExported :: Bool
+    } deriving (Eq, Show)
+data TopLevelDeclaration =
+    ImportDeclaration String ImportListing |
+    ExportDeclaration Destructuring |
+    ValueDeclaration DeclarationModule (Maybe Expression) (Maybe TypeExpression) |
+    TypeDeclaration DeclarationModule TypeExpression deriving (Eq, Show)
