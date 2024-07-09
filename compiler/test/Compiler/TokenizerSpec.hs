46b85ac75894a45947c2b96fb463d87505114962
Caleb Stimpson <caleb.stimpson@gmail.com>
2024-07-08 21:34:10 -0500

    Tokenize parenthesis alone

--- compiler/test/Compiler/TokenizerSpec.hs
+++ compiler/test/Compiler/TokenizerSpec.hs
@@ -3,18 +3,31 @@ module Compiler.TokenizerSpec (spec) where
 import Test.Hspec
 import Compiler.Tokenizer (tokenize, kind, content, Token(Token), TokenKind(..))
 
+spec :: SpecWith ()
 spec = describe "Compiler.Tokenizer" $ do
-    it "export main:IO()\\t=of()\\n" $ do
-        tokenize "export main:IO()\t=of()\n" `shouldBe` [
-                Token { kind = LetterIdentifier, content = "export" },
-                Token { kind = InlineWhitespace, content = " " },
-                Token { kind = LetterIdentifier, content = "main" },
-                Token { kind = SymbolIdentifier, content = ":" },
-                Token { kind = LetterIdentifier, content = "IO" },
-                Token { kind = SymbolIdentifier, content = "()" },
-                Token { kind = InlineWhitespace, content = "\t" },
-                Token { kind = SymbolIdentifier, content = "=" },
-                Token { kind = LetterIdentifier, content = "of" },
-                Token { kind = SymbolIdentifier, content = "()" },
-                Token { kind = EOL, content = "\n" }
-            ]
+  it "export main:IO()\\t=of()\\n" $ do
+    tokenize "export main:IO()\t=of()\n" `shouldBe`
+      [ Token { kind = LetterIdentifier, content = "export" }
+      , Token { kind = InlineWhitespace, content = " " }
+      , Token { kind = LetterIdentifier, content = "main" }
+      , Token { kind = SymbolIdentifier, content = ":" }
+      , Token { kind = LetterIdentifier, content = "IO" }
+      , Token { kind = SymbolIdentifier, content = "(" }
+      , Token { kind = SymbolIdentifier, content = ")" }
+      , Token { kind = InlineWhitespace, content = "\t" }
+      , Token { kind = SymbolIdentifier, content = "=" }
+      , Token { kind = LetterIdentifier, content = "of" }
+      , Token { kind = SymbolIdentifier, content = "(" }
+      , Token { kind = SymbolIdentifier, content = ")" }
+      , Token { kind = EOL, content = "\n" }
+      ]
+  it "should tokenize parenthesis alone" $ do
+    let symbol a = Token { kind = SymbolIdentifier, content = a }
+    tokenize ")::())(" `shouldBe`
+      [ symbol ")"
+      , symbol "::"
+      , symbol "("
+      , symbol ")"
+      , symbol ")"
+      , symbol "("
+      ]
