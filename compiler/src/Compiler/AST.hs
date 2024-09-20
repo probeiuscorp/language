eacac80f763e500c65e39aed3bcc26fef3903c95
Caleb Stimpson <caleb.stimpson@gmail.com>
2024-09-20 14:02:33 -0600

    style: Reformat AST.hs

--- compiler/src/Compiler/AST.hs
+++ compiler/src/Compiler/AST.hs
@@ -4,52 +4,58 @@ type ValidIdentifier = String
 type PropertyKey = String
 
 data Abstraction = Abstraction
-    { binding :: PropertyKey
-    , constraint :: Maybe TypeExpression
-    , body :: Expression
-    } deriving (Eq, Show)
+  { binding :: PropertyKey
+  , constraint :: Maybe TypeExpression
+  , body :: Expression
+  } deriving (Eq, Show)
 data CallArgs a = CallArgs
-    { target :: a
-    , argument :: a
-    } deriving (Eq, Show)
+  { target :: a
+  , argument :: a
+  } deriving (Eq, Show)
 type ExpressionCallArgs = CallArgs Expression
 type TypeExpressionCallArgs = CallArgs TypeExpression
-data Expression =
-    Function Abstraction |
-    Application ExpressionCallArgs |
-    Identifier ValidIdentifier|
-    StringLiteral String |
-    NumberLiteral Double |
-    Record [Abstraction] deriving (Eq, Show)
+data Expression
+  = Function Abstraction
+  | Application ExpressionCallArgs
+  | Identifier ValidIdentifier
+  | StringLiteral String
+  | NumberLiteral Double
+  | Record [Abstraction]
+  deriving (Eq, Show)
 
-data TypeExpression =
-    TypeConstructor Abstraction |
-    TypeApplication TypeExpressionCallArgs |
-    TypeIdentifier ValidIdentifier |
-    TypeofExpression Expression deriving (Eq, Show)
+data TypeExpression
+  = TypeConstructor Abstraction
+  | TypeApplication TypeExpressionCallArgs
+  | TypeIdentifier ValidIdentifier
+  | TypeofExpression Expression
+  deriving (Eq, Show)
 
-data Associativity = LeftAssociative | RightAssociative
-    deriving (Eq, Show)
+data Associativity = LeftAssociative | RightAssociative deriving (Eq, Show)
 data InfixDeclaration = InfixDeclaration
-    { precedence :: Double
-    , infixIdent :: ValidIdentifier
-    , associativity :: Associativity
-    } deriving (Eq, Show)
+  { precedence :: Double
+  , infixIdent :: ValidIdentifier
+  , associativity :: Associativity
+  } deriving (Eq, Show)
 
 data DestructuringMember = DestructuringMember
-    { destructIdent :: ValidIdentifier
-    , destructBinding :: ValidIdentifier
-    } deriving (Eq, Show)
+  { destructIdent :: ValidIdentifier
+  , destructBinding :: ValidIdentifier
+  } deriving (Eq, Show)
 type Destructuring = [DestructuringMember]
-data ImportListing = ImportAll | ImportOnly Destructuring | ImportHiding Destructuring | ImportAs ValidIdentifier
-    deriving (Eq, Show)
+data ImportListing
+  = ImportAll
+  | ImportOnly Destructuring
+  | ImportHiding Destructuring
+  | ImportAs ValidIdentifier
+  deriving (Eq, Show)
 
 data DeclarationModule = DeclarationModule
-    { identifier :: ValidIdentifier
-    , isExported :: Bool
-    } deriving (Eq, Show)
-data TopLevelDeclaration =
-    ImportDeclaration String ImportListing |
-    ExportDeclaration Destructuring |
-    ValueDeclaration DeclarationModule (Maybe Expression) (Maybe TypeExpression) |
-    TypeDeclaration DeclarationModule TypeExpression deriving (Eq, Show)
+  { identifier :: ValidIdentifier
+  , isExported :: Bool
+  } deriving (Eq, Show)
+data TopLevelDeclaration
+  = ImportDeclaration String ImportListing
+  | ExportDeclaration Destructuring
+  | ValueDeclaration DeclarationModule (Maybe Expression) (Maybe TypeExpression)
+  | TypeDeclaration DeclarationModule TypeExpression
+  deriving (Eq, Show)
