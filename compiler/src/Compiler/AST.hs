beeb36e85044238ce50ffc0975b574de012c098e
Caleb Stimpson <caleb.stimpson@gmail.com>
2024-09-30 17:33:33 -0600

    Simplify AST
    I will probably revert most of these changes over time but right now a
    simpler AST will let me start serious work on the parser.

--- compiler/src/Compiler/AST.hs
+++ compiler/src/Compiler/AST.hs
@@ -1,34 +1,21 @@
 module Compiler.AST where
 
 type ValidIdentifier = String
-type PropertyKey = String
 
-data Abstraction = Abstraction
-  { binding :: PropertyKey
-  , constraint :: Maybe TypeExpression
-  , body :: Expression
-  } deriving (Eq, Show)
-data CallArgs a = CallArgs
-  { target :: a
-  , argument :: a
-  } deriving (Eq, Show)
-type ExpressionCallArgs = CallArgs Expression
-type TypeExpressionCallArgs = CallArgs TypeExpression
+data Destructuring
+  = DestructBind ValidIdentifier
+  | DestructCons (ValidIdentifier, [Destructuring])
+  | DestructRecord [(ValidIdentifier, Destructuring)]
+  deriving (Eq, Show)
 data Expression
-  = Function Abstraction
-  | Application ExpressionCallArgs
-  | Identifier ValidIdentifier
-  | StringLiteral String
-  | NumberLiteral Double
-  | Record [Abstraction]
+  = ExFunction [Destructuring] Expression
+  | ExApplication Expression Expression
+  | ExIdentifier ValidIdentifier
+  | ExStringLiteral String
+  | ExNumberLiteral Double
+  | ExRecord [(String, Expression)]
   deriving (Eq, Show)
-
-data TypeExpression
-  = TypeConstructor Abstraction
-  | TypeApplication TypeExpressionCallArgs
-  | TypeIdentifier ValidIdentifier
-  | TypeofExpression Expression
-  deriving (Eq, Show)
+data TypeExpression = TypeExpression deriving (Eq, Show)
 
 data Associativity = LeftAssociative | RightAssociative deriving (Eq, Show)
 data InfixDeclaration = InfixDeclaration
@@ -37,15 +24,10 @@ data InfixDeclaration = InfixDeclaration
   , associativity :: Associativity
   } deriving (Eq, Show)
 
-data DestructuringMember = DestructuringMember
-  { destructIdent :: ValidIdentifier
-  , destructBinding :: ValidIdentifier
-  } deriving (Eq, Show)
-type Destructuring = [DestructuringMember]
 data ImportListing
   = ImportAll
-  | ImportOnly Destructuring
-  | ImportHiding Destructuring
+  | ImportOnly [(ValidIdentifier, Expression)]
+  | ImportHiding [(ValidIdentifier, Expression)]
   | ImportAs ValidIdentifier
   deriving (Eq, Show)
 
@@ -55,8 +37,8 @@ data DeclarationModule = DeclarationModule
   } deriving (Eq, Show)
 data TopLevelDeclaration
   = ImportDeclaration String ImportListing
-  | ExportDeclaration Destructuring
-  | DataDeclaration String [TypeExpression]
+  | ExportDeclaration [(ValidIdentifier, Expression)]
+  | DataDeclaration ValidIdentifier [TypeExpression]
   | ValueDeclaration DeclarationModule (Maybe Expression) (Maybe TypeExpression)
   | TypeDeclaration DeclarationModule TypeExpression
   deriving (Eq, Show)
