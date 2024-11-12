5279a53244a74baa71bfcc7bd2f7589adc157810
Caleb Stimpson <caleb.stimpson@gmail.com>
2024-11-11 18:48:35 -0700

    Convert TokenizerSpec to snapshot testing

--- compiler/test/Compiler/TokenizerSpec.hs
+++ compiler/test/Compiler/TokenizerSpec.hs
@@ -1,57 +1,15 @@
 module Compiler.TokenizerSpec (spec) where
 
 import Test.Hspec
-import Compiler.Tokenizer (tokenize, kind, content, Token(Token), TokenKind(..))
+import Compiler.Tokenizer (tokenize)
+import Compiler.SnapshotTesting (snapshotData)
+import Data.List (intercalate)
 
 spec :: SpecWith ()
 spec = describe "Compiler.Tokenizer" $ do
-  it "export main:IO()\\t=of()\\n" $ do
-    tokenize "export main:IO()\t=of()\n" `shouldBe`
-      [ Token { kind = LetterIdentifier, content = "export" }
-      , Token { kind = InlineWhitespace, content = " " }
-      , Token { kind = LetterIdentifier, content = "main" }
-      , Token { kind = SymbolIdentifier, content = ":" }
-      , Token { kind = LetterIdentifier, content = "IO" }
-      , Token { kind = SymbolIdentifier, content = "(" }
-      , Token { kind = SymbolIdentifier, content = ")" }
-      , Token { kind = InlineWhitespace, content = "\t" }
-      , Token { kind = SymbolIdentifier, content = "=" }
-      , Token { kind = LetterIdentifier, content = "of" }
-      , Token { kind = SymbolIdentifier, content = "(" }
-      , Token { kind = SymbolIdentifier, content = ")" }
-      , Token { kind = EOL, content = "\n" }
-      ]
-  let symbol a = Token { kind = SymbolIdentifier, content = a }
-  it "should tokenize parenthesis alone" $ do
-    tokenize ")::())(" `shouldBe`
-      [ symbol ")"
-      , symbol "::"
-      , symbol "("
-      , symbol ")"
-      , symbol ")"
-      , symbol "("
-      ]
-  it "should tokenize braces alone" $ do
-    tokenize "}::{)}{" `shouldBe`
-      [ symbol "}"
-      , symbol "::"
-      , symbol "{"
-      , symbol ")"
-      , symbol "}"
-      , symbol "{"
-      ]
-  it "should tokenize math symbols alone" $ do
-    tokenize "∀x. x + x==2x" `shouldBe`
-      [ Token { kind = SymbolIdentifier, content = "∀" }
-      , Token { kind = LetterIdentifier, content = "x" }
-      , Token { kind = SymbolIdentifier, content = "." }
-      , Token { kind = InlineWhitespace, content = " " }
-      , Token { kind = LetterIdentifier, content = "x" }
-      , Token { kind = InlineWhitespace, content = " " }
-      , Token { kind = SymbolIdentifier, content = "+" }
-      , Token { kind = InlineWhitespace, content = " " }
-      , Token { kind = LetterIdentifier, content = "x" }
-      , Token { kind = SymbolIdentifier, content = "==" }
-      , Token { kind = NumberLiteral, content = "2" }
-      , Token { kind = LetterIdentifier, content = "x" }
-      ]
+  let prettyTokenize = intercalate "\n" . fmap show . tokenize
+  let test testCase contents = snapshotData "tokenize/" testCase (show contents, prettyTokenize contents)
+  test "simple example" "export main:IO()\t=of()\n"
+  test "parens" ")::())("
+  test "braces" "}::{)}{"
+  test "math symbols" "∀x. x + x==2x"
