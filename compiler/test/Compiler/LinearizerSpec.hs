4b0389980a1b2480e202a59506535c3c8ff6306e
Caleb Stimpson <caleb.stimpson@gmail.com>
2024-11-13 16:48:15 -0700

    Format output of LinearizerSpec for readability

--- compiler/test/Compiler/LinearizerSpec.hs
+++ compiler/test/Compiler/LinearizerSpec.hs
@@ -2,14 +2,28 @@ module Compiler.LinearizerSpec (spec) where
 
 import Test.Hspec
 import qualified Compiler.Zipper as Z
-import Compiler.Linearizer (linearize)
+import Compiler.Linearizer (linearize, Linearized, GLinearized (..))
 import Compiler.Tokenizer (tokenize, content)
 import Compiler.SnapshotTesting (snapshot)
 
+prettyPrintLinearization :: [Linearized] -> String
+prettyPrintLinearization l = "[\n" ++ go "" l ++ "]"
+  where
+    go :: String -> [Linearized] -> String
+    go indent lbody = lbody >>= prettyPrintLinearized ("  " ++ indent)
+    prettyPrintLinearized :: String -> Linearized -> String
+    prettyPrintLinearized indent t@(LinToken _) = indent ++ (show . fmap content) t ++ ",\n"
+    prettyPrintLinearized indent (LinFunction lparams lbody) = indent ++ "LinFunction" ++ showLinearization indent lparams ++ showLinearization indent lbody ++ ",\n"
+    prettyPrintLinearized indent (LinParens lbody)   = indent ++ "LinParens" ++   showLinearization indent lbody ++ ",\n"
+    prettyPrintLinearized indent (LinBraces lbody)   = indent ++ "LinBraces" ++   showLinearization indent lbody ++ ",\n"
+    prettyPrintLinearized indent (LinBrackets lbody) = indent ++ "LinBrackets" ++ showLinearization indent lbody ++ ",\n"
+    showLinearization :: String -> [Linearized] -> String
+    showLinearization indent lbody = " [\n" ++ go indent lbody ++ indent ++ "]"
+
 spec :: SpecWith ()
 spec = describe "linearize" $ do
-  let prettyPrintLinearization = show . fmap (fmap content) . linearize . Z.start . tokenize
-  let test = snapshot "linearize/" prettyPrintLinearization
+  let prettyParseLinearization = prettyPrintLinearization . linearize . Z.start . tokenize
+  let test = snapshot "linearize/" prettyParseLinearization
   test "parens" "a (b c d) e"
   test "nested parens" "a ((b c) d) e"
   test "braces" "a {b c d} e"
